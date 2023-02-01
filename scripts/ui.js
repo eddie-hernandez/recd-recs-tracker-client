7fa0502fc2cf0fb03c83be27b8ae7880dbde55cd
eddie-hernandez <71785228+eddie-hernandez@users.noreply.github.com>
2023-02-01 09:38:18 -0500

    formatting doc

--- scripts/ui.js
+++ scripts/ui.js
@@ -108,141 +108,141 @@ export const onShowRecordSuccess = (record) => {
     <h2 id="show-record-title">Selected Album</h2>
     <h3 id="show-record"><b>"${record.albumTitle}"</b></h3>
     <h5>Artist Name: <b>${record.artistName}</b></h5>
-        <h5>Year Released: <b>${record.yearReleased}</b></h5>
-        <h5>Comments: <b><i>"${record.comments}"</i></b></h5></br>
-        
-        <div class="record-form-container">
+    <h5>Year Released: <b>${record.yearReleased}</b></h5>
+    <h5>Comments: <b><i>"${record.comments}"</i></b></h5></br>
+    
+    <div class="record-form-container">
         <button class="btn btn-light" data-bs-toggle="collapse" data-bs-target="#record-form" aria-expanded="false" aria-controls="row" id="edit-record-button">Edit Record Info</button></br></br>
         <form id="record-form" class="collapse" data-id="${record._id}">
         <div class="form-floating">
-        <input type="text" name="albumTitle" id="albumTitle" value="${record.albumTitle}" class="form-control" />
-        <label for="albumTitle" class="form-label" class="form-control">Album Title</label>
+            <input type="text" name="albumTitle" id="albumTitle" value="${record.albumTitle}" class="form-control" />
+            <label for="albumTitle" class="form-label" class="form-control">Album Title</label>
+        </div>
+            <div class="form-floating">
+            <input type="text" name="artistName" id="artistName" value="${record.artistName}" class="form-control" />
+            <label for="artistName" class="form-label" class="form-control">Artist Name</label>
         </div>
         <div class="form-floating">
-        <input type="text" name="artistName" id="artistName" value="${record.artistName}" class="form-control" />
-        <label for="artistName" class="form-label" class="form-control">Artist Name</label>
+            <input type="number" name="yearReleased" id="yearReleased" value="${record.yearReleased}" class="form-control" />
+            <label for="yearReleased" class="form-label" class="form-control">Year Released</label>
         </div>
         <div class="form-floating">
-        <input type="number" name="yearReleased" id="yearReleased" value="${record.yearReleased}" class="form-control" />
-        <label for="yearReleased" class="form-label" class="form-control">Year Released</label>
-        </div>
-        <div class="form-floating">
-        <input type="text" name="comments" id="comments" value="${record.comments}" class="form-control" />
-        <label for="comments" class="form-label" class="form-control">Comments (if any)</label>
+            <input type="text" name="comments" id="comments" value="${record.comments}" class="form-control" />
+            <label for="comments" class="form-label" class="form-control">Comments (if any)</label>
         </div></br>
         <button type="submit" class="btn btn-success" value="Update Record Info" />Update Record Info</button></br></br>
         </form>
         </div>
         <button type="button" class="delete-record btn btn-danger" value="Delete Record" data-id="${record._id}" />Delete Record</button></br>
-        </div>
-        `
+    </div>
+    `
+    
+    showRecordContainer.appendChild(recordDiv)
         
-        showRecordContainer.appendChild(recordDiv)
         
         
+    if (record.linerNotes.length == 1) {
+        deleteLinerNoteContainer.style.display = "block"
+        const linerNoteDiv = document.createElement('div')
+        linerNoteDiv.innerHTML = `
+        <h2 id="show-liner-note"><i><u>Liner Notes</u>:</i></h2>
+        <h5>Album Rating: <b>${record.linerNotes[0].rating}/10</b></h5>
+        <h5>Standout Track: <b>"${record.linerNotes[0].standoutTrack}"</b></h5>
+        <h5>Thoughts: <b><i>"${record.linerNotes[0].thoughts}"</i></b></h5></br>
         
-        if (record.linerNotes.length == 1) {
-            deleteLinerNoteContainer.style.display = "block"
-            const linerNoteDiv = document.createElement('div')
-            linerNoteDiv.innerHTML = `
-            <h2 id="show-liner-note"><i><u>Liner Notes</u>:</i></h2>
-            <h5>Album Rating: <b>${record.linerNotes[0].rating}/10</b></h5>
-            <h5>Standout Track: <b>"${record.linerNotes[0].standoutTrack}"</b></h5>
-            <h5>Thoughts: <b><i>"${record.linerNotes[0].thoughts}"</i></b></h5></br>
-            
-            <button class="btn btn-light" data-bs-toggle="collapse" data-bs-target=".update-liner-note-form" aria-expanded="false" aria-controls="row" id="edit-liner-note-button">Edit Liner Note Info</button></br></br>
-            <form class="update-liner-note-form collapse" data-id="${record.linerNotes[0]._id}">
-            <div class="form-floating">
+        <button class="btn btn-light" data-bs-toggle="collapse" data-bs-target=".update-liner-note-form" aria-expanded="false" aria-controls="row" id="edit-liner-note-button">Edit Liner Note Info</button></br></br>
+        <form class="update-liner-note-form collapse" data-id="${record.linerNotes[0]._id}">
+        <div class="form-floating">
             <input type="number" name="rating" id="rating" value="${record.linerNotes[0].rating}" class="form-control" />
             <label for="rating" class="form-label" class="form-control">Record Rating (1-10)</label>
-            </div>
-            <div class="form-floating">
+        </div>
+        <div class="form-floating">
             <input type="text" name="standoutTrack" id="standoutTrack" value="${record.linerNotes[0].standoutTrack}" class="form-control" />
             <label for="standoutTrack" class="form-label" class="form-control">Standout Track</label>
-            </div>
-            <div class="form-floating">
+        </div>
+        <div class="form-floating">
             <input type="text" name="thoughts" id="thoughts" value="${record.linerNotes[0].thoughts}" class="form-control" />
             <label for="thoughts" class="form-label" class="form-control">Thoughts? (if any)</label>
-            </div>
-            <div class="form-floating">
+        </div>
+        <div class="form-floating">
             <input type="text" name="recordId" id="recordId" value="${record._id}" class="form-control" disabled />
             <label for="recordId" class="form-label" class="form-control">Record ID</label>
-            </div></br>
-            <button type="submit" class="btn btn-success" value="Update Liner Note" />Update Liner Note Info for "${record.albumTitle}"</button>
-            </form>
-            `
-            linerNoteFormContainer.appendChild(linerNoteDiv)
-            
-            const deleteLinerNoteDiv = document.createElement('div')
-            deleteLinerNoteDiv.innerHTML = `
-            <div data-id="${record._id}" id="delete-container">
-            <button type="button" class="delete-liner-note btn btn-danger" value="Delete Liner Note" data-id="${record.linerNotes[0]._id}" />Delete Liner Note</button></br>
-            </div>
-            `
-            deleteLinerNoteContainer.appendChild(deleteLinerNoteDiv)
-            
-        }
+        </div></br>
+        <button type="submit" class="btn btn-success" value="Update Liner Note" />Update Liner Note Info for "${record.albumTitle}"</button>
+        </form>
+        `
+        linerNoteFormContainer.appendChild(linerNoteDiv)
         
-        else {
-            deleteLinerNoteContainer.style.display = "none"
-            const linerNoteDiv = document.createElement('div')
-            linerNoteDiv.innerHTML = `
-            <h4 id="show-liner-note"><b><i>Finished Listening?</i></b></h4></br>
-            <button class="btn btn-light" data-bs-toggle="collapse" data-bs-target=".create-liner-note-form" aria-expanded="false" aria-controls="row" id="create-liner-note-button">Create New Liner Note</button></br></br>
-            <form class="create-liner-note-form collapse">
-            <div class="form-floating">
+        const deleteLinerNoteDiv = document.createElement('div')
+        deleteLinerNoteDiv.innerHTML = `
+        <div data-id="${record._id}" id="delete-container">
+            <button type="button" class="delete-liner-note btn btn-danger" value="Delete Liner Note" data-id="${record.linerNotes[0]._id}" />Delete Liner Note</button></br>
+        </div>
+        `
+        deleteLinerNoteContainer.appendChild(deleteLinerNoteDiv)
+        
+    }
+        
+    else {
+        deleteLinerNoteContainer.style.display = "none"
+        const linerNoteDiv = document.createElement('div')
+        linerNoteDiv.innerHTML = `
+        <h4 id="show-liner-note"><b><i>Finished Listening?</i></b></h4></br>
+        <button class="btn btn-light" data-bs-toggle="collapse" data-bs-target=".create-liner-note-form" aria-expanded="false" aria-controls="row" id="create-liner-note-button">Create New Liner Note</button></br></br>
+        <form class="create-liner-note-form collapse">
+        <div class="form-floating">
             <input type="number" name="rating" id="rating" placeholder="add rating here" class="form-control" />
             <label for="rating" class="form-label" class="form-control">Record Rating (1-10)</label>
-            </div>
-            <div class="form-floating">
+        </div>
+        <div class="form-floating">
             <input type="text" name="standoutTrack" id="standoutTrack" placeholder="add standout track here" class="form-control" />
             <label for="standoutTrack" class="form-label" class="form-control">Standout Track</label>
-            </div>
-            <div class="form-floating">
+        </div>
+        <div class="form-floating">
             <input type="text" name="thoughts" id="thoughts" placeholder="add any thoughts on the record here" class="form-control" />
             <label for="thoughts" class="form-label" class="form-control">Thoughts? (if any)</label>
-            </div>
-            <div class="form-floating">
+        </div>
+        <div class="form-floating">
             <input type="text" name="recordId" id="recordId" value="${record._id}" class="form-control" disabled />
             <label for="recordId" class="form-label" class="form-control">Record ID</label>
-            </div></br>
-            <button type="submit" class="btn btn-success" value="Add Liner Note" />Add Liner Note to "${record.albumTitle}"</button></br>
-            </form>
-            `
-            
-            linerNoteFormContainer.appendChild(linerNoteDiv)
-        }
-    }
-    
-    export const onCreateRecordSuccess = () => {
-        recordMessageContainer.innerHTML = `<h4 id="record-message" class="col-8 offset-2"><i>now THAT'S a record!</i></h4>`
-        setTimeout(() => {recordMessageContainer.innerHTML = ``}, 1500)
-        
-    }
-    
-    export const onUpdateRecordSuccess = () => {
-        recordMessageContainer.innerHTML = `<h4 id="record-message" class="col-8 offset-2"><i>record successfully updated!</i></h4>`
-        setTimeout(() => {recordMessageContainer.innerHTML = ``}, 1500)
-    }
-    
-    export const onDeleteRecordSuccess = () => {
-        recordMessageContainer.innerHTML = `<h4 id="record-message" class="col-8 offset-2"><i>record deletion successful!</i></h4>`
-        setTimeout(() => {recordMessageContainer.innerHTML = ``}, 1500)
-    }
-    
-    export const onRecordFailure = (error) => {
-        recordMessageContainer.innerHTML = `
-            <h4 id="record-message" class="col-8 offset-2">woah! you've got a record error!</h4>
-            <p id="error" class="col-8 offset-2">${error}</p>
+        </div></br>
+        <button type="submit" class="btn btn-success" value="Add Liner Note" />Add Liner Note to "${record.albumTitle}"</button></br>
+        </form>
         `
-        setTimeout(() => {recordMessageContainer.innerHTML = ``}, 2500)
+        
+        linerNoteFormContainer.appendChild(linerNoteDiv)
     }
+}
+    
+export const onCreateRecordSuccess = () => {
+    recordMessageContainer.innerHTML = `<h4 id="record-message" class="col-8 offset-2"><i>now THAT'S a record!</i></h4>`
+    setTimeout(() => {recordMessageContainer.innerHTML = ``}, 1500)
+    
+}
+
+export const onUpdateRecordSuccess = () => {
+    recordMessageContainer.innerHTML = `<h4 id="record-message" class="col-8 offset-2"><i>record successfully updated!</i></h4>`
+    setTimeout(() => {recordMessageContainer.innerHTML = ``}, 1500)
+}
+
+export const onDeleteRecordSuccess = () => {
+    recordMessageContainer.innerHTML = `<h4 id="record-message" class="col-8 offset-2"><i>record deletion successful!</i></h4>`
+    setTimeout(() => {recordMessageContainer.innerHTML = ``}, 1500)
+}
+
+export const onRecordFailure = (error) => {
+    recordMessageContainer.innerHTML = `
+        <h4 id="record-message" class="col-8 offset-2">woah! you've got a record error!</h4>
+        <p id="error" class="col-8 offset-2">${error}</p>
+    `
+    setTimeout(() => {recordMessageContainer.innerHTML = ``}, 2500)
+}
     
     
-    // LINER NOTES
+// LINER NOTES
     
-    export const onLinerNoteFailure = (error) => {
-        recordMessageContainer.innerHTML = `
+export const onLinerNoteFailure = (error) => {
+    recordMessageContainer.innerHTML = `
         <h4 id="record-message" class="col-8 offset-2">woah! you've got a liner note error!</h4>
         <p id="error" class="col-8 offset-2">${error}</p>
     `
